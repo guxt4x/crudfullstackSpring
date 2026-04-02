1a98dff017c5db28277d263dc8b2fe1a0facaef1
Milton Junior <miltonfsjunior@gmail.com>
2026-04-01 21:17:40 -0300

    revert: remover entidades Professor, Disciplina, Turma, Matricula e Avaliacao e seus artefatos

--- backend/src/main/java/com/exemplo/crudmongo/repository/AvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.crudmongo.repository;
-
-import com.exemplo.crudmongo.Model.Avaliacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
-}
